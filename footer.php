150d8d523ff9cf8ba19c286ae31bdc7d9b099ff5
Alicia <izquierdogarcia@gmail.com>
2013-04-29 18:11:12 +0200

    Créditos

--- footer.php
+++ footer.php
@@ -28,13 +28,7 @@
 				</a>
 			</div><!-- #site-info -->
 
-			<div id="site-generator">
-				<?php do_action( 'twentyten_credits' ); ?>
-				<a href="<?php echo esc_url( __('http://wordpress.org/', 'twentyten') ); ?>"
-						title="<?php esc_attr_e('Semantic Personal Publishing Platform', 'twentyten'); ?>" rel="generator">
-					<?php printf( __('Proudly powered by %s.', 'twentyten'), 'WordPress' ); ?>
-				</a>
-			</div><!-- #site-generator -->
+			
 
 		</div><!-- #colophon -->
 	</footer><!-- #footer -->
